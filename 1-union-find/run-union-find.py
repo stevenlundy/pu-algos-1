e091ba4616f600512557fe976ea35be30150d81e
Steven Lundy <steven.lundy@gmail.com>
2016-01-29 21:06:09 -0500

    Add file to run test data

---
+++ 1-union-find/run-union-find.py
@@ -0,0 +1,14 @@
+from quickfind import UF
+
+uf = UF(10)
+uf.union(4, 3)
+uf.union(3, 8)
+uf.union(6, 5)
+uf.union(9, 4)
+uf.union(2, 1)
+print(uf.connected(8, 9))
+uf.union(5, 0)
+uf.union(7, 2)
+uf.union(6, 1)
+print(uf.connected(1, 0))
+print(uf.connected(6, 7))
