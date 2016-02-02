272b5eeef52aec4fee443e2567edfb8785b59c20
Steven Lundy <steven.lundy@gmail.com>
2016-02-02 18:45:08 -0500

    Add test file for stack

---
+++ 2-stacks-queues/stack-test.py
@@ -0,0 +1,19 @@
+from stack import Stack
+
+s = Stack()
+
+print s.pop() # None
+s.push('to')
+s.push('be')
+s.push('or')
+s.push('not')
+s.push('to')
+print s.pop() # to
+s.push('be')
+print s.pop() # be
+print s.pop() # not
+s.push('that')
+print s.pop() # that
+print s.pop() # or
+print s.pop() # be
+s.push('is')
