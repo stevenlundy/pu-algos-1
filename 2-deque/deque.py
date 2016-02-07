3000c5afe60820e16f7cb16c5213bb0930650009
Steven Lundy <steven.lundy@gmail.com>
2016-02-07 18:29:35 -0500

    Create Deque constructor

---
+++ 2-deque/deque.py
@@ -0,0 +1,25 @@
+class Deque:
+  def __init__(self):
+    '''construct an empty deque'''
+
+  def isEmpty(self):
+    '''is the deque empty?'''
+
+  def size(self):
+    '''return the number of items on the deque'''
+
+  def addFirst(self, item):
+    '''add the item to the front'''
+
+  def addLast(self, item):
+    '''add the item to the end'''
+
+  def removeFirst(self):
+    '''remove and return the item from the front'''
+
+  def removeLast(self):
+    '''remove and return the item from the end'''
+
+  def __iter__(self):
+
+  def next(self):
